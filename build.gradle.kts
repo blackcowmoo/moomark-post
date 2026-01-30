2d2da2246ba265f1fd36de3a007a0e55163f6346
mykim <38449976+Apisapple@users.noreply.github.com>
2026-01-31 01:05:21 +0900

    chore: Update Gradle configuration for Kotlin and Spring Boot versions and add lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
-    id("org.springframework.boot") version "2.5.2"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "2.7.18"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.5.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.5.21"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.7.21"
     id("jacoco")
+    id("org.jlleitschuh.gradle.ktlint") version "11.4.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
     checkstyle
 }
 
@@ -46,7 +48,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -55,10 +57,14 @@ tasks.test {
     finalizedBy(tasks.jacocoTestReport)
 }
 
+tasks.named("check") {
+    dependsOn(tasks.named("ktlintCheck"))
+}
+
 checkstyle {
     toolVersion = "8.26"
     configFile = file("./checkstyle.xml")
-    reportsDir = file("${buildDir}/checkstyle-output")
+    reportsDir = file("$buildDir/checkstyle-output")
 }
 
 tasks.checkstyleMain {
@@ -76,5 +82,17 @@ tasks.jacocoTestReport {
     reports {
         xml.required.set(true)
         html.required.set(true)
+    }
+}
+
+configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+    debug.set(false)
+    verbose.set(true)
+    android.set(false) // Android 프로젝트가 아니면 false
+    outputToConsole.set(true)
+    ignoreFailures.set(false) // 린트 에러 발생 시 빌드 실패 처리 (권장)
+    enableExperimentalRules.set(true) // 최신 규칙 적용 여부
+    filter {
+        exclude("**/generated/**") // 생성된 파일 제외
     }
 }
