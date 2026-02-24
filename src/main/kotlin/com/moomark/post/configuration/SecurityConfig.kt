6aa99de184fb97e62dc29f9b88c5085beb46c8e4
mykim <38449976+Apisapple@users.noreply.github.com>
2026-02-24 11:42:32 +0900

    refactor: Migrate SecurityConfig to use SecurityFilterChain and improve configuration

--- src/main/kotlin/com/moomark/post/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/moomark/post/configuration/SecurityConfig.kt
@@ -2,20 +2,22 @@ package com.moomark.post.configuration
 
 import com.moomark.post.configuration.passport.PassportFilter
 import com.moomark.post.configuration.passport.PassportService
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 class SecurityConfig(
     private val passportService: PassportService,
-) : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
+) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -36,7 +38,7 @@ class SecurityConfig(
             .permitAll()
             .antMatchers(HttpMethod.GET, "/api/v1/posts/count")
             .permitAll()
-            .antMatchers(HttpMethod.GET, "/api/v1/post/{postId}")
+            .antMatchers(HttpMethod.GET, "/api/v1/post/*")
             .permitAll()
             .antMatchers(HttpMethod.GET, "/actuator/health")
             .permitAll()
@@ -47,5 +49,7 @@ class SecurityConfig(
             PassportFilter(passportService),
             UsernamePasswordAuthenticationFilter::class.java,
         )
+
+        return http.build()
     }
 }
